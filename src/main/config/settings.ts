8a5946123821cf3ebfeb28253140617bfb6c13f8
Devin Binnie <devin.binnie@mattermost.com>
2022-07-17 14:31:28 -0400

    Oops

---
+++ src/main/config/settings.ts
@@ -0,0 +1,74 @@
+import fs from 'fs';
+import path from 'path';
+
+import {app} from 'electron';
+import log from 'electron-log';
+
+import {Settings, SettingsWithVM, VM} from 'types/config';
+
+class ManagerSettings {
+    settings?: SettingsWithVM;
+
+    init = () => {
+        try {
+            const rawConfig = JSON.parse(fs.readFileSync(path.join(app.getPath('userData'), 'config.json'), 'utf8'));
+            this.settings = rawConfig as SettingsWithVM;
+            return true;
+        } catch (err) {
+            log.error('Cannot load config, using defaults.', err);
+            this.settings = {
+                vms: [],
+                exePath: '',
+                cfgPath: path.join(app.getPath('home'), '86BoxVMs'),
+                showConsole: true,
+                logging: false,
+                logPath: '',
+                //sortColumn: 0,
+                //sortOrder: 'asc',
+            }
+            if (!this.writeConfig()) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    writeConfig = () => {
+        try {
+            const configString = JSON.stringify(this.settings);
+            fs.writeFileSync(path.join(app.getPath('userData'), 'config.json'), configString);
+            return true;
+        } catch (err) {
+            log.error('Cannot write config', err);
+            return false;
+        }
+    }
+
+    setConfig = (config: Settings) => {
+        this.settings = {
+            vms: [],
+            ...this.settings,
+            ...config,
+        };
+        if (!this.writeConfig()) {
+            return false;
+        }
+        return true;
+    }
+
+    addVM = (vm: VM) => {
+        if (!this.settings) {
+            log.error('Cannot add VM when config is not initialized');
+            return;
+        }
+
+        this.settings.vms.push(vm);
+        if (!this.writeConfig()) {
+            return false;
+        }
+        return true;
+    }
+}
+
+const managerSettings = new ManagerSettings();
+export default managerSettings;
